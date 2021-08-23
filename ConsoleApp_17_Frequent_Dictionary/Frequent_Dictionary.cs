897e7395721cece9258ee11185e4316cbb36cadf
ItCodingOFF <onlinegta484@gmail.com>
2021-08-24 00:12:38 +0300

    2 commit

--- ConsoleApp_17_Frequent_Dictionary/Frequent_Dictionary.cs
+++ ConsoleApp_17_Frequent_Dictionary/Frequent_Dictionary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,125 @@ namespace ConsoleApp_17_Frequent_Dictionary
 {
 	class Frequent_Dictionary
 	{
-	}
+        string dirpath; string[] files;
+        Dictionary<string, int> words = new Dictionary<string, int>();
+
+        public Frequent_Dictionary(string path)
+        {
+            this.dirpath = path;
+            this.files = Directory.GetFiles(this.dirpath);
+        }
+
+        public Dictionary<string, int> ReadText()
+        {
+
+            char[] symbols = { ' ', ',', '.', '!', '?', ':', '"', '*', '(', ')', '$', '`', '^', '%' };
+            foreach (var item in files)
+            {
+                if (item.Contains(".txt"))
+                {
+                    string[] word1 = File.ReadAllLines(item);
+                    foreach (string t in word1)
+                    {
+                        string[] word2 = t.Split(symbols);
+                        foreach (var item1 in word2)
+                        {
+                            if (words.ContainsKey(item1))
+                            {
+                                words[item1]++;
+                            }
+                            else
+                            {
+                                words.Add(item1, 1);
+                            }
+                        }
+                    }
+                }
+
+            }
+            return this.words;
+        }
+        //Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+        //public int OnEvent(HashSet<string> files)
+        //{
+
+        //    foreach (var item in files)
+        //    {
+
+        //        string[] words = File.ReadAllLines(item);
+        //        foreach (var item1 in words)
+        //        {
+        //            Regex regex = new Regex(@"\w*(( )|(,)|(.)|(1))");
+        //            MatchCollection matches = regex.Matches(item1.ToLower());
+        //            foreach (Match item2 in matches)
+        //            {
+        //                if (dictionary.ContainsKey(item2.Value))
+        //                {
+        //                    dictionary[item2.Value]++;
+        //                }
+        //                else
+        //                {
+        //                    dictionary.Add(item2.Value, 1);
+        //                }
+
+
+        //            }
+        //        }
+
+        //    }
+        //    using (StreamWriter sw = new StreamWriter("Word.txt", false, System.Text.Encoding.Default))
+        //    {
+        //        foreach (var item in dictionary)
+        //        {
+        //            sw.WriteLine(item.Key + " " + item.Value);
+
+        //        }
+
+        //    }
+        //    foreach (var item in dictionary)
+        //    {
+        //        Console.WriteLine(item.Key + " " + item.Value);
+        //    }
+        //    return 0;
+        //}
+
+        public IOrderedEnumerable<KeyValuePair<string, int>> SortDescending(Dictionary<string, int> SD)
+        {
+            return SD.OrderByDescending(i => i.Value);
+        }
+        public int SaveToFile(IOrderedEnumerable<KeyValuePair<string, int>> words, string savefile)
+        {
+            //Text file = new Text();
+            ////- Save - сохранение текста в файл
+            //public void Save(string path)
+            //{
+            //    BinaryFormatter binFormat = new BinaryFormatter();
+            //    using (Stream fStream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None))
+            //    {
+            //        binFormat.Serialize(fStream, file);
+
+            //    }
+            //}
+            try
+            {
+                StreamWriter SW = new StreamWriter(savefile + "\\WordResult.txt");
+                foreach (var item in words)
+                {
+                    byte[] array = System.Text.Encoding.Default.GetBytes(item.Key + " " + item.Value); SW.WriteLine(item.ToString());
+                }
+                SW.Close();
+                Console.WriteLine("[ Word count finish ]");
+
+                return 0;
+            }
+			catch (Exception)
+			{
+
+				return 1;
+			}
+
+		}
+
+    }
 }
--- ConsoleApp_17_Frequent_Dictionary/Program.cs
+++ ConsoleApp_17_Frequent_Dictionary/Program.cs
@@ -9,7 +9,22 @@ namespace ConsoleApp_17_Frequent_Dictionary
 	class Program
 	{
 		static void Main(string[] args)
-		{
-		}
+        {
+            Console.Title = "Examen (Frequent Dictionary)";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("[ Enter path ]");
+            string dirpath = Console.ReadLine();
+
+            Frequent_Dictionary t = new Frequent_Dictionary(dirpath);
+            Dictionary<string, int> v = t.ReadText();
+            IOrderedEnumerable<KeyValuePair<string, int>> word = t.SortDescending(v);
+
+            Console.WriteLine("Enter path for file result: ");
+            string savefile = Console.ReadLine();
+            t.SaveToFile(word, savefile);
+            //D:\WordsTEst
+        }
 	}
 }
