7d5ff42305188c1f9a0818cc3b7d539ab284f32c
ItCodingOFF <onlinegta484@gmail.com>
2021-08-25 02:25:38 +0300

    new UnitTesti

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,12 +1,68 @@
 using System;
 using Xunit;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+
 
 namespace TestProject
 {
+	
 	public class UnitTest1
 	{
 		[Fact]
 		public void Test1()
+		{
+			string path = @"C:\Temp";
+			Frequent_Dictionary t = new Frequent_Dictionary(path);
+			if (t == null)
+			{
+				Assert.Inconclusive();
+			}
+		}
+		[Fact]
+		public void Test2()
+		{
+
+		}
+		[Fact]
+		public void Test3()
+		{
+
+		}
+		[Fact]
+		public void Test4()
+		{
+
+		}
+		[Fact]
+		public void Test5()
+		{
+
+		}
+		[Fact]
+		public void Test6()
+		{
+
+		}
+		[Fact]
+		public void Test7()
+		{
+
+		}
+		[Fact]
+		public void Test8()
+		{
+
+		}
+		[Fact]
+		public void Test9()
+		{
+
+		}
+		[Fact]
+		public void Test10()
 		{
 
 		}
